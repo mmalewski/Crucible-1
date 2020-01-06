1d22669da22d377e285c80687fd64f0e50f8b15c
InX-Invader <InX-Invader@users.noreply.github.com>
2020-01-06 00:03:35 -0600

    Revert "Revert "Initial commit""
    This reverts commit 9ced5111fcf3cf722983db82c5cc2b32c358741b.

--- Crucible/MainWindow.xaml.cs
+++ Crucible/MainWindow.xaml.cs
@@ -211,10 +211,13 @@ namespace Crucible
             }
 
             ReadPerforceInformation();
+
+            //Unneed perforce information check for a supported changelist, going to remove ~ Invader
+/*
             if (Perforce != null)
             {
                 const Int64 minSupportedChangelist = 0;
-                const Int64 maxSupportedChangelist = 1209915;
+                const Int64 maxSupportedChangelist = 3917223;
 
                 bool isVersionUnsupported = false;
                 isVersionUnsupported |= Perforce.RequestedP4ChangeNum < minSupportedChangelist;
@@ -227,7 +230,7 @@ namespace Crucible
                     return;
                 }
             }
-
+*/
             this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(delegate ()
             {
                 this.P4KFilesystemTab.Content = new FilesystemView(FilesystemManager.P4KFilesystem);
